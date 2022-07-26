b7e818a2b349531a6d3ac7a854b228eecc3e9504
Pablo Buitrago <pablo@briefboards.com>
2022-07-26 20:26:28 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
+  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -15,15 +11,10 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        mocha: true
-      }
+  overrides: [{
+    files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+    env: {
+      mocha: true
     }
-  ]
-}
+  }]
+};
