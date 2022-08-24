9fbcdf1b24049c4748e25a89fec0167e77c9d52d
Pablo Buitrago <pablo@briefboards.com>
2022-08-23 23:22:40 -0300

    Updated Verifive L&F.

--- src/components/TheVerifiveConnect.vue
+++ src/components/TheVerifiveConnect.vue
@@ -7,11 +7,22 @@
         </el-col>
       </el-row>
       <el-row class="veri-banner-body veri-banner-background">
-        <el-col :xs="12" :sm="12">
+        <el-col :xs="10" :sm="10">
           <h2 class="veri-title">{{ company.name }}</h2>
           <p class="veri-intro-text">Adding the final stich to the Identity Trust Fabric</p>
           <el-divider border-style="none"/>
           <the-verida-connect-button :namespace="namespace"/>
+        </el-col>
+        <el-col :offset="3" :xs="11" :sm="11" class="veri-banner-sign">
+          <p>
+            In the analog world, we are used to checking accreditations: Physicians, lawyers and even chefs have their diplomas framed and hanging on the walls of their waiting rooms. You probably checked that the school you enrolled into was accredited, unless it is a well known university.
+          </p>
+          <p>
+            At the same time, many people and organisations try to take advantage of the trust most people have in their fellow humans by passing themselves as something they are not. Trump university was famously not accredited as an educational organisation. Many invoices have been issued to a company that has never been incorporated.
+          </p>
+          <p>
+            <strong style="color: black;">How can WE as a society detect FRAUD?</strong>
+          </p>
         </el-col>
       </el-row>
       <el-row class="veri-body veri-body-background">
@@ -64,6 +75,15 @@ const company = computed(() => storeNamespace.company.profile)
   padding: 20px;
   height: 50vh;
 }
+.veri-banner-sign {
+  background-color: rgb(223, 132, 67);
+  color: whitesmoke;
+  padding: 10px;
+  font-size: 12px;
+  text-align: center;
+  letter-spacing: 1px;
+  font-family: Helvetica;
+}
 .veri-title {
   font-family: 'ui-serif';
   font-size: 42px;
@@ -73,7 +93,7 @@ const company = computed(() => storeNamespace.company.profile)
 }
 .veri-intro-text {
   font-family: 'Roboto';
-  font-size: 22px;
+  font-size: 18px;
   color: whitesmoke;
 }
 .veri-banner-background {
@@ -96,7 +116,7 @@ const company = computed(() => storeNamespace.company.profile)
   height: 25vh;
 }
 .veri-body-background {
-  background-color: #df8444
+  background-color: #1e2020
 }
 .veri-footer {
   color: whitesmoke;
